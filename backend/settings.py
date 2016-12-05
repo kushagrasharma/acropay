b967568567f6ead99d66135b18b899dc87448cbf
Kushagra Sharma <ksharm01@gmail.com>
2016-12-04 22:42:10 -0500

    deleted file

--- backend/settings.py
+++ backend/settings.py
@@ -1,1 +1,5 @@
-DOMAIN = {'people': {}}
+S = ParametricPlot3D[ (Cos[t/2]^2 + Sin[s/2] + 2*Cos[s]*Sin[t]) {Cos[t] Sin[s], 
+     Sin[t] Sin[s], Cos[s]}/2, {t, 0, 2 Pi}, {s, 0, Pi}, 
+  PlotStyle -> Directive[Opacity[0.7], Blue, Specularity[White, 10]], 
+  MeshStyle -> {{Yellow, Thickness[0.001]}, {Orange, 
+     Thickness[0.001]}}, Axes -> False, Boxed -> False]
