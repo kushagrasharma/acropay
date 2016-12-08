a65b912ee1fd7e9e82de09de464f3b15d493f4b0
Kushagra Sharma <ksharm01@gmail.com>
2016-12-08 10:14:12 -0500

    finished bullshit backend

--- backend/settings.py
+++ backend/settings.py
@@ -1,21 +1,44 @@
 schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'barcode': {
+        'type': 'string',
+        'unique': True
+    },
     'price': {
-        'type': 'number',
-        'required': True
+        'type': 'numeric',
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
     },
     'name': {
-        'type': 'string',
-        'required': True
+        'type': 'string'
     }
 }
 
 products = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'product',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
     'additional_lookup': {
         'url': 'regex("[\w]+")',
-        'field': 'price'
+        'field': 'barcode'
     },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET'],
+
     'schema': schema
 }
+
 DOMAIN = {
     'products': products,
 }
