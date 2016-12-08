110b4bf18f7d6733c7cfc3c4482379e9c48d2107
Kushagra Sharma <ksharm01@gmail.com>
2016-12-08 08:32:40 -0500

    backend edits

--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,10 @@
-DOMAIN = {'products': {}}
+products = {
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'price'
+    }
+}
+
+DOMAIN = {'products': {products}}
 
 MONGO_DBNAME = 'products'
