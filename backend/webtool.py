2b0f9672a4fc30102c70db6868de4d248844dc15
kushagrasharma <ksharm01@gmail.com>
2016-12-08 22:50:49 +0000

    hello

--- backend/webtool.py
+++ backend/webtool.py
@@ -6,7 +6,7 @@ from flask import current_app as app
 class MyBasicAuth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource,
                    method):
-        return username == 'client' and password == 'cs50final123'
+        return username == 'admin' and password == 'secret'
 
 app = Eve(auth=MyBasicAuth)
 
