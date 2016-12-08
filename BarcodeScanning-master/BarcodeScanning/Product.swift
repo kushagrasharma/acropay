09e0c2f7f5aaa2c591eb6cbdbd944a389cf47eb3
Kushagra Sharma <ksharm01@gmail.com>
2016-12-08 16:54:28 -0500

    linking back and front

--- BarcodeScanning-master/BarcodeScanning/Product.swift
+++ BarcodeScanning-master/BarcodeScanning/Product.swift
@@ -22,13 +22,20 @@ class Product: NSObject{
     private var password = "cs50final123"
     
     init(name: String = "Oreos", serialNumber: String = "0044000007492", valueInDollars: Double = 2.75, quantity: Int = 1) {
-        let JSONData 
-        self.name = name
-        self.priceInDollars = round(100*valueInDollars)/100
         self.barcodeNumber = serialNumber
-        self.dateCreated = Date()
-        self.quantity = quantity
-        
+        self.quantity = 1
+        if let JSONData = getDataFromServer(barcode: serialNumber) as? JSON{
+            self.name = name
+            self.priceInDollars = round(100*valueInDollars)/100
+            self.dateCreated = Date()
+
+        }
+        else{
+            self.name = name
+            self.priceInDollars = round(100*valueInDollars)/100
+            self.dateCreated = Date()
+
+        }
         super.init()
     }
     
@@ -38,13 +45,11 @@ class Product: NSObject{
     
     // http://stackoverflow.com/questions/24379601/how-to-make-an-http-request-basic-auth-in-swift
     func getDataFromServer(barcode: String) -> JSON?{
+        var json:JSON?
         Alamofire.request("http://acropay.io/products/\(barcode)")
                  .authenticate(user: self.user, password: self.password)
                  .responseJSON { response in
-            if((response.result.value) != nil) {
-                return JSON(responseData.result.value!)
-            }
-            return nil
+            json = response.result.value
         }
     }
 }
