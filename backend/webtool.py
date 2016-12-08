24644bcff442a288db2ad8bcefeceb5b42f50ce7
Kushagra Sharma <ksharm01@gmail.com>
2016-12-08 16:25:39 -0500

    added basic auth

--- backend/webtool.py
+++ backend/webtool.py
@@ -1,7 +1,14 @@
-
+import bcrypt
 from eve import Eve
-app = Eve()
+from eve.auth import BasicAuth
+from flask import current_app as app
+
+class MyBasicAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource,
+                   method):
+        return username == 'admin' and password == 'secret'
+
+app = Eve(auth=MyBasicAuth)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
